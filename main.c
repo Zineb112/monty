69f445b7c49569a9b32f6642c8c5075569ee849e
kawakibb <kawakibk7@gmail.com>
2023-07-27 14:02:39 -0700

    man.c

--- main.c
+++ main.c
@@ -1,70 +1,46 @@
 #include "monty.h"
-
+bus_t bus = {NULL, NULL, NULL, 0};
 /**
- * main - check code.
- * @argc: command line arguments.
- * @argv: command line arguments string array.
- * Return: 0 on success, non-zero on failure.
- */
+* main - monty code_interpreter
+* @argc: nbr of arg
+* @argv: monty file loction
+* Return: 0 if  success
+*/
 int main(int argc, char *argv[])
 {
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        return (EXIT_FAILURE);
-    }
+	char *contnt;
+	FILE *file;
+	size_t size = 0;
+	ssize_t read_line = 1;
+	stack_t *stack = NULL;
+	unsigned int cntr = 0;
 
-    FILE *file = fopen(argv[1], "r");
-    if (file == NULL)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        return (EXIT_FAILURE);
-    }
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	file = fopen(argv[1], "r");
+	bus.file = file;
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (read_line > 0)
+	{
+		contnt = NULL;
+		read_line = getline(&contnt, &size, file);
+		bus.contnt = contnt;
+		cntr++;
+		if (read_line > 0)
+		{
+			execute(contnt, &stack, cntr, file);
+		}
+		free(contnt);
+	}
+	free_stack(stack);
+	fclose(file);
+return (0);
+}
 
-    char opcode[100];
-    int value;
-    unsigned int line_number = 1;
-
-    instruction_t instructions[] = {
-        {"push", push},
-        {"pall", pall},
-        {NULL, NULL}
-    };
-
-    while (fscanf(file, "%s", opcode) != EOF)
-    {
-        if (strcmp(opcode, "push") == 0)
-        {
-            if (fscanf(file, "%d", &value) != 1)
-            {
-                fprintf(stderr, "L%d: usage: push integer\n", line_number);
-                fclose(file);
-                free_stack(stack);
-                return (EXIT_FAILURE);
-            }
-            push(&stack, value, line_number);
-        }
-        else
-        {
-            int i = 0;
-            while (instructions[i].opcode != NULL)
-            {
-                if (strcmp(opcode, instructions[i].opcode) == 0)
-                {
-                    instructions[i].f(&stack, line_number);
-                    break;
-                }
-                i++;
-            }
-            if (instructions[i].opcode == NULL)
-            {
-                fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
-                fclose(file);
-                free_stack(stack);
-                return (EXIT_FAILURE);
-            }
-        }
-        line_number++;
-    }
-
-    fclose(file);
