35be8a6166fcc96b457cc36393e89cdf4bd09468
Zineb112 <zbelassiri@gmail.con>
2023-07-27 14:00:12 -0700

    header file updated2

--- monty.h
+++ monty.h
@@ -1,5 +1,5 @@
-#ifndef _monty_h_
-#define _monty_h_
+#ifndef MONTY_H
+#define MONTY_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,11 +8,6 @@
 #include <fcntl.h>
 #include <string.h>
 #include <ctype.h>
-#include <string.h>
-
-#define EXIT_SUCCESS 0
-#define EXIT_FAILURE 1
-
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -30,7 +25,22 @@ typedef struct stack_s
 	struct stack_s *next;
 } stack_t;
 
-
+/**
+ * struct bus_s - variables -args, file, line contnt
+ * @arg: value
+ * @file: pointer to monty file
+ * @contnt: line contnt
+ * @flg_lifi: flag change stack <-> queue
+ * Description: carries values of the program
+ */
+typedef struct bus_s
+{
+	char *arg;
+	FILE *file;
+	char *contnt;
+	int flg_lifi;
+}  bus_t;
+extern bus_t bus;
 
 /**
  * struct instruction_s - opcode and its function
@@ -38,58 +48,37 @@ typedef struct stack_s
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct instruction_s
 {
 	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
+	void (*f)(stack_t **stack, unsigned int line_nbr);
 } instruction_t;
 
-
-/**
- * struct bus_s - variables -args, file, line content
- * @arg: value
- * @file: pointer to monty file
- * @content: line content
- * @lifi: flag change stack <-> queue
- * Description: carries values through the program
- */
-typedef struct bus_s
-{
-	char *arg;
-	FILE *file;
-	char *content;
-	int lifi;
-}  bus_t;
-
-extern bus_t bus;
-
-/* Function prototypes */
-void f_mul(stack_t **head, unsigned int counter);
-void f_mod(stack_t **head, unsigned int counter);
-void f_pchar(stack_t **head, unsigned int counter);
-void f_pstr(stack_t **head, unsigned int counter);
-void f_rotl(stack_t **head, unsigned int counter);
-void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter);
-void addnode(stack_t **head, int n);
-void addqueue(stack_t **head, int n);
-void f_queue(stack_t **head, unsigned int counter);
-void f_stack(stack_t **head, unsigned int counter);
 char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
-ssize_t getstdin(char **lineptr, int file);
-char  *clean_line(char *content);
-void f_push(stack_t **head, unsigned int number);
-void f_pall(stack_t **head, unsigned int number);
-void f_pint(stack_t **head, unsigned int number);
-int execute(char *content, stack_t **head, unsigned int counter, FILE *file);
-void free_stack(stack_t *head);
-void f_pop(stack_t **head, unsigned int counter);
-void f_swap(stack_t **head, unsigned int counter);
-void f_add(stack_t **head, unsigned int counter);
-void f_nop(stack_t **head, unsigned int counter);
-void f_sub(stack_t **head, unsigned int counter);
-void f_div(stack_t **head, unsigned int counter);
-
+char  *clean_line(char *contnt);
+void my_push(stack_t **head_stk, unsigned int nbr);
+void my_pall(stack_t **head_stk, unsigned int nbr);
+void my_pint(stack_t **head_stk, unsigned int nbr);
+int execute(char *contnt, stack_t **head_stk, unsigned int cntr, FILE *file);
+void free_stack(stack_t *head_stk);
+void my_pop(stack_t **head_stk, unsigned int cntr);
+void my_swap(stack_t **head_stk, unsigned int cntr);
+void my_add(stack_t **head_stk, unsigned int cntr);
+void my_nop(stack_t **head_stk, unsigned int cntr);
+void my_sub(stack_t **head_stk, unsigned int cntr);
+void my_div(stack_t **head_stk, unsigned int cntr);
+void my_mul(stack_t **head_stk, unsigned int cntr);
+void my_mod(stack_t **head_stk, unsigned int cntr);
+void my_pchar(stack_t **head_stk, unsigned int cntr);
+void my_pstr(stack_t **head_stk, unsigned int cntr);
+void my_rotl(stack_t **head_stk, unsigned int cntr);
+void my_rotr(stack_t **head_stk, __attribute__((unused)) unsigned int cntr);
+void add_nod(stack_t **head_stk, int n);
+void add_qu(stack_t **head_stk, int n);
+void my_queue(stack_t **head_stk, unsigned int cntr);
+void my_stack(stack_t **head_stk, unsigned int cntr);
 
 #endif
+
